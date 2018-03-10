fd3bd71d6ba324ee62d7da1ee3205662ede73ddb
reece baird <reeceybaird@hotmail.com>
2018-03-10 21:32:50 +0100

    added new command

--- slack-lunchbot/commands/calculate.rb
+++ slack-lunchbot/commands/calculate.rb
@@ -4,6 +4,14 @@ module SlackLunchbot
       command 'Hey' do |client, data, _match|
         client.say(channel: data.channel, text: 'Wazaaaaaaap')
       end
+
+      command 'start' do |client, data, _match|
+        peopleGoing = 0
+        client.say(channel: data.channel, text: 'If you are going to lunch at 13:00 today, Please leave a comment below saying Yes!' + peopleGoing)
+        peopleGoing += 1
+
+      end
+
     end
 
 
