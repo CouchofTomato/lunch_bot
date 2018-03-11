56fdef19d28f9f15fa0fc4f76f5e9fb76c0f3491
reece baird <reeceybaird@hotmail.com>
2018-03-11 14:49:38 +0100

    Yes command takes username, stores in array and slack bot displays names in array

--- slack-lunchbot/commands/calculate.rb
+++ slack-lunchbot/commands/calculate.rb
@@ -1,19 +1,24 @@
 module SlackLunchbot
   module Commands
     class Talk < SlackRubyBot::Commands::Base
+
       command 'Hey' do |client, data, _match|
         client.say(channel: data.channel, text: 'Wazaaaaaaap')
       end
 
       command 'start' do |client, data, _match|
-        peopleGoing = 0
-        client.say(channel: data.channel, text: 'If you are going to lunch at 13:00 today, Please leave a comment below saying Yes!' + peopleGoing)
-        peopleGoing += 1
-
+        client.say(channel: data.channel, text: "#{data.user}")
       end
 
+      command 'Yes' do |client, data, _match|
+        arr = []
+        arr << "<@#{data.user}>"
+        client.say(channel: data.channel, text: arr)
+        puts "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+        puts arr
+      end
+
+
     end
-
-
   end
 end
