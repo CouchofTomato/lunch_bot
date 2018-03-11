a3ec3397e1a68f8b0d2179927b7cfb5ae7dcb913
reece baird <reeceybaird@hotmail.com>
2018-03-11 15:24:43 +0100

    command stop gathers users in array and splits into even groups

--- slack-lunchbot/commands/calculate.rb
+++ slack-lunchbot/commands/calculate.rb
@@ -2,6 +2,8 @@ module SlackLunchbot
   module Commands
     class Talk < SlackRubyBot::Commands::Base
 
+      arr = []
+
       command 'Hey' do |client, data, _match|
         client.say(channel: data.channel, text: 'Wazaaaaaaap')
       end
@@ -11,11 +13,18 @@ module SlackLunchbot
       end
 
       command 'Yes' do |client, data, _match|
-        arr = []
+
         arr << "<@#{data.user}>"
         client.say(channel: data.channel, text: arr)
         puts "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
         puts arr
+        puts "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+        puts arr
+      end
+
+      command 'stop' do |client, data, _match|
+        newArr = arr.each_slice(1).to_a
+        client.say(channel: data.channel, text: newArr)
       end
 
 
